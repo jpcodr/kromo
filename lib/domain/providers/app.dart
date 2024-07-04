39576f146916ed0d367bcfdb6e4998a33c7c3332
Pablo Macias <jupamara@gmail.com>
2024-07-04 13:29:00 -0600

    docs: Remove some logs.

--- lib/domain/providers/app.dart
+++ lib/domain/providers/app.dart
@@ -1,13 +1,10 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-// import 'package:kromo/domain/providers/audio.dart';
 import 'package:kromo/domain/providers/preferences.dart';
 
 final appStateProvider = FutureProvider<void>((ref) async {
   ref.onDispose(() {
     ref.invalidate(sharedPrefsProvider);
-    // ref.invalidate(audioProvider);
   });
 
   await ref.watch(sharedPrefsProvider.future);
-  // await ref.watch(audioProvider.future);
 });
